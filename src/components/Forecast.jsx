624af25ca3439f0659703146dbaf215503e0e439
Abhayyyyyyyy <abhaydange89@gmail.com>
2024-02-05 04:19:25 +0530

    update days

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -11,7 +11,7 @@ function Forecast({title, items}) {
         <div className='flex flex-row items-center justify-between text-white'>
             {items.map(item =>(
                 <div className='flex flex-col items-center justify-center'>
-                <p className='font-light text-sm'>4:30 PM</p>
+                <p className='font-light text-sm'>{item.title}</p>
                 <img src={iconUrlFromCode(item.icon)} alt="" className='w-12 my-1' />
                 <p className='font-medium'>{`${item.temp.toFixed()}°`}</p>
             </div>
